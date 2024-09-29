9b5d888243f104882a4b1313cad53a9e103475a0
风吟 <3160257358@qq.com>
2024-09-30 02:32:12 +0800

    1

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from core import  PictureCutting,Translate
 if __name__=="__main__":
     p=PictureCutting("asserts/image.png")
     pic_path=p.recognize_cutting()
-    t = Translate(pic_path,id="AKIDru3GUiyIe0OhuxpcGqEVbjegYO9lmaBY",key="JQM5ez73wc6MOp36ygDfw54rTxGPPFuO")
+    t = Translate(pic_path,id=,key=)
     re = t.Translate_string()
     print(re[0], re[1])
